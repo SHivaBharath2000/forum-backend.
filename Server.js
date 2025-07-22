12b79c461d746d576313428c6f729c41067975d9
siva <bharathsiva411@gmail.com>
2025-07-22 23:19:01 +0530

    add commit

--- Server.js
+++ Server.js
@@ -2,12 +2,14 @@ import express from "express";
 import connectDB from "./DButils/connection.js";
 import registerRouter from "./Api's/register.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app=express();
 const PORT=process.env.port;
 //middleware
 app.use(express.json());
+app.use(cors());
 app.use((req,res,next)=>{
     console.log(`${req.method} ${req.url}`);
     next()
